8fb633bf8dd896966ce37ffe217e8b61e926ba45
Praveen Thirumurugan <mail@praveent.com>
2025-09-09 23:05:12 +0530

    chore: remove staticDirs from Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,6 @@ import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  staticDirs: ["./public"],
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
     "@storybook/addon-docs",
